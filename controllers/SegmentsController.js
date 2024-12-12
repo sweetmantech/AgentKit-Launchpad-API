242418140ba3aacaa73088411dfdc84800e7738b
Va~~~dym <tech.eng.me@gmail.com>
2024-12-11 16:13:20 -0800

    add: dev log

--- controllers/SegmentsController.js
+++ controllers/SegmentsController.js
@@ -62,11 +62,10 @@ export const get_next_steps = async (req, res) => {
 export const get_segments = async (req, res) => {
   try {
     const body = req.body;
-    console.log("ZIAD", body)
     const content = await getChatCompletions([
       {
         role: "user",
-        content: `Context: `,
+        content: `Context: ${JSON.stringify(body)}`,
       },
       {
         role: "system",
