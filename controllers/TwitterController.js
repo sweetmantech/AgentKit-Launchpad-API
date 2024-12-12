e0a61c9d39865920f85bc57cc08068a7a8da2a0f
Va~~~dym <tech.eng.me@gmail.com>
2024-12-12 04:16:46 -0800

    add: get tweets

--- controllers/TwitterController.js
+++ controllers/TwitterController.js
@@ -1,4 +1,6 @@
 import { Scraper, SearchMode } from "agent-twitter-client";
+import { MAX_TWEETS } from "../lib/consts";
+import processTweetData from "../lib/twitter/processTweetData";
 
 const scraper = new Scraper();
 
@@ -10,6 +12,46 @@ export const getProfile = async (req, res) => {
     return res.status(200).json({ profile });
   } catch (error) {
     console.error(error);
+    return res.status(500).json({ error });
+  }
+};
+
+export const getAllTweets = async (req, res) => {
+  const profile = req.body;
+  const allTweets = new Map();
+  let previousCount = 0;
+  let stagnantBatches = 0;
+  const MAX_STAGNANT_BATCHES = 2;
+
+  try {
+    const searchResults = scraper.searchTweets(
+      `from:${profile.username}`,
+      MAX_TWEETS,
+      SearchMode.Latest,
+    );
+
+    for await (const tweet of searchResults) {
+      if (tweet && !allTweets.has(tweet.id)) {
+        const processedTweet = processTweetData(tweet);
+        if (processedTweet) {
+          allTweets.set(tweet.id, processedTweet);
+
+          if (allTweets.size % 100 === 0) {
+            if (allTweets.size === previousCount) {
+              stagnantBatches++;
+              if (stagnantBatches >= MAX_STAGNANT_BATCHES) {
+                break;
+              }
+            } else {
+              stagnantBatches = 0;
+            }
+            previousCount = allTweets.size;
+          }
+        }
+      }
+    }
+    return res.status(200).json({ tweets: Array.from(allTweets.values()) });
+  } catch (error) {
     return res.status(500).json({ error });
   }
 };
