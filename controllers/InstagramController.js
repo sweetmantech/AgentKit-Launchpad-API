c13f19465e1f143582e285cde551081f934bf0ee
Va~~~dym <tech.eng.me@gmail.com>
2024-12-19 11:24:05 -0800

    add: global controller

--- controllers/InstagramController.js
+++ controllers/InstagramController.js
@@ -1,10 +1,5 @@
-import getActorStatus from "../lib/apify/getActorStatus.js";
-import getDataset from "../lib/apify/getDataset.js";
 import runTikTokActor from "../lib/apify/runTikTokActor.js";
-import {
-  OUTSTANDING_ERROR,
-  UNKNOWN_PROFILE_ERROR,
-} from "../lib/twitter/errors.js";
+import { OUTSTANDING_ERROR } from "../lib/twitter/errors.js";
 
 export const get_instagram_account_profile = async (req, res) => {
   const { handle } = req.query;
@@ -24,34 +19,6 @@ export const get_instagram_account_profile = async (req, res) => {
         res.status(500).json({ error: OUTSTANDING_ERROR });
     }
     return res.status(200).json({ success: true, data: response });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error });
-  }
-};
-
-export const get_dataset_items = async (req, res) => {
-  const { datasetId } = req.query;
-
-  try {
-    const data = await getDataset(datasetId);
-    if (data?.[0]?.error === UNKNOWN_PROFILE_ERROR)
-      return res.status(500).json({ error: UNKNOWN_PROFILE_ERROR });
-    return res.status(200).json({ success: true, data });
-  } catch (error) {
-    return res.status(500).json({ error });
-  }
-};
-
-export const get_dataset_status = async (req, res) => {
-  const { datasetId } = req.query;
-
-  try {
-    const data = await getActorStatus(datasetId);
-    return res.status(200).json({
-      success: true,
-      data,
-    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error });
