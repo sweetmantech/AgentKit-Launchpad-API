92c2ae8132fd6b29e7aa08e3831671168b7434df
Va~~~dym <tech.eng.me@gmail.com>
2024-12-19 01:26:43 -0800

    chore: remove dev logs

--- controllers/SpotifyController.js
+++ controllers/SpotifyController.js
@@ -8,7 +8,13 @@ export const getProfile = async (req, res) => {
   if (artist?.error) return res.status(500).json({ error: artist?.error });
   try {
     return res.status(200).json({
-      profile: artist,
+      profile: {
+        name: artist.name,
+        image: artist.images?.[0]?.url || "",
+        fans: artist.followers.total,
+        id: artist.id,
+        external_urls: artist.external_urls,
+      },
     });
   } catch (error) {
     console.error(error);
