3a2865b2aa77c84a8d9f97de38f7958d5c807539
Va~~~dym <tech.eng.me@gmail.com>
2024-12-22 18:19:57 -0800

    fix: tiktok analysis

--- agents/getTikTokAnalysis.js
+++ agents/getTikTokAnalysis.js
@@ -38,7 +38,7 @@ const getTikTokAnalysis = async (handle, chat_id, account_id) => {
       `https://tiktok.com/@${profile?.name}`,
       account_id,
     );
-    console.log("ZIAD", artistId);
+    console.log("ZIAD ARTIST ID", artistId);
     global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.SAVING_ANALYSIS });
     const analysis = {
       ...profileWithComments,
