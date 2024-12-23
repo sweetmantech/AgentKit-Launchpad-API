8a11c82a2a69470c652cbc72f94cfe379aba1165
Va~~~dym <tech.eng.me@gmail.com>
2024-12-23 12:03:11 -0800

    feat: funnel supabase

--- agents/getTikTokAnalysis.js
+++ agents/getTikTokAnalysis.js
@@ -3,8 +3,11 @@ import getSegments from "../lib/getSegments.js";
 import getSegmentsWithIcons from "../lib/getSegmentsWithIcons.js";
 import uploadPfpToIpfs from "../lib/ipfs/uploadPfpToIpfs.js";
 import { STEP_OF_ANALYSIS } from "../lib/step.js";
-import saveFunnelAnalysis from "../lib/supabase/saveFunnelAnalysis.js";
+import beginAnalysis from "../lib/supabase/beginAnalysis.js";
+import completeAnalysis from "../lib/supabase/completeAnalysis.js";
 import saveFunnelArtist from "../lib/supabase/saveFunnelArtist.js";
+import saveFunnelComments from "../lib/supabase/saveFunnelComments.js";
+import saveFunnelProfile from "../lib/supabase/saveFunnelProfile.js";
 import getProfile from "../lib/tiktok/getProfile.js";
 import getProfileDatasetId from "../lib/tiktok/getProfileDatasetId.js";
 import getVideoComments from "../lib/tiktok/getVideoComments.js";
@@ -12,22 +15,31 @@ import getVideoComments from "../lib/tiktok/getVideoComments.js";
 const getTikTokAnalysis = async (handle, chat_id, account_id) => {
   try {
     console.log("ZIAD START");
+    const newAnalysis = await beginAnalysis(chat_id);
+    const analysisId = newAnalysis.id;
+    console.log("ZIAD NEW ANALYSIS", newAnalysis);
     const profileDatasetId = await getProfileDatasetId(handle);
     console.log("ZIAD PROFILE DATASET ID", profileDatasetId);
     global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.PROFILE });
     const profile = await getProfile(profileDatasetId);
     console.log("ZIAD PROFILE", profile);
-    const videoComments = await getVideoComments(profile?.videos, chat_id);
     const avatar = await uploadPfpToIpfs(profile.avatar);
-    console.log("ZIAD AVATAR", avatar);
-    const profileWithComments = {
+    const analytics_profile = await saveFunnelProfile({
       ...profile,
       avatar,
-      videos: videoComments.videos,
-      total_video_comments_count: videoComments.total_video_comments_count,
-    };
+      type: "TIKTOK",
+      analysis_id: analysisId,
+    });
+    console.log("ZIAD ANALYTICS PROFILE", analytics_profile);
+    const videoComments = await getVideoComments(
+      profile?.videos,
+      chat_id,
+      analysisId,
+    );
+    await saveFunnelComments(videoComments);
+    console.log("ZIAD AVATAR", avatar);
     global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.SEGMENTS });
-    const segments = await getSegments(profileWithComments);
+    const segments = await getSegments(videoComments);
     const segmentsWithIcons = await getSegmentsWithIcons(segments);
     console.log("ZIAD SEGMENTS", segmentsWithIcons);
     global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.CREATING_ARTIST });
@@ -40,13 +52,7 @@ const getTikTokAnalysis = async (handle, chat_id, account_id) => {
     );
     console.log("ZIAD ARTIST ID", artistId);
     global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.SAVING_ANALYSIS });
-    const analysis = {
-      ...profileWithComments,
-      segments: [...segmentsWithIcons],
-      chat_id,
-      artistId,
-    };
-    await saveFunnelAnalysis(analysis);
+    await completeAnalysis(analysisId);
     return;
   } catch (error) {
     console.log(error);
