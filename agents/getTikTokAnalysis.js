514fab070c8dbc193a5860bad250c017b2502079
Va~~~dym <tech.eng.me@gmail.com>
2024-12-24 00:20:06 -0800

    fix: analysis status

--- agents/getTikTokAnalysis.js
+++ agents/getTikTokAnalysis.js
@@ -5,11 +5,11 @@ import uploadPfpToIpfs from "../lib/ipfs/uploadPfpToIpfs.js";
 import trackFunnelAnalysisChat from "../lib/stack/trackFunnelAnalysisChat.js";
 import { STEP_OF_ANALYSIS } from "../lib/step.js";
 import beginAnalysis from "../lib/supabase/beginAnalysis.js";
-import completeAnalysis from "../lib/supabase/completeAnalysis.js";
 import saveFunnelArtist from "../lib/supabase/saveFunnelArtist.js";
 import saveFunnelComments from "../lib/supabase/saveFunnelComments.js";
 import saveFunnelProfile from "../lib/supabase/saveFunnelProfile.js";
 import saveFunnelSegments from "../lib/supabase/saveFunnelSegments.js";
+import updateAnalysisStatus from "../lib/supabase/updateAnalysisStatus.js";
 import getProfile from "../lib/tiktok/getProfile.js";
 import getProfileDatasetId from "../lib/tiktok/getProfileDatasetId.js";
 import getVideoComments from "../lib/tiktok/getVideoComments.js";
@@ -19,7 +19,7 @@ const getTikTokAnalysis = async (handle, chat_id, account_id, address) => {
     const newAnalysis = await beginAnalysis(chat_id);
     const analysisId = newAnalysis.id;
     const profileDatasetId = await getProfileDatasetId(handle);
-    global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.PROFILE });
+    await updateAnalysisStatus(chat_id, analysisId, STEP_OF_ANALYSIS.PROFILE);
     const accountData = await getProfile(profileDatasetId);
     const profile = accountData?.profile?.[0];
     const videoUrls = accountData?.videoUrls;
@@ -30,11 +30,15 @@ const getTikTokAnalysis = async (handle, chat_id, account_id, address) => {
       analysisId,
     );
     await saveFunnelComments(videoComments);
-    global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.SEGMENTS });
+    await updateAnalysisStatus(chat_id, analysisId, STEP_OF_ANALYSIS.SEGMENTS);
     const segments = await getSegments(videoComments);
     const segmentsWithIcons = await getSegmentsWithIcons(segments, analysisId);
     await saveFunnelSegments(segmentsWithIcons);
-    global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.CREATING_ARTIST });
+    await updateAnalysisStatus(
+      chat_id,
+      analysisId,
+      STEP_OF_ANALYSIS.CREATING_ARTIST,
+    );
     const newArtist = await saveFunnelArtist(
       Funnel_Type.TIKTOK,
       profile?.nickname,
@@ -50,7 +54,11 @@ const getTikTokAnalysis = async (handle, chat_id, account_id, address) => {
       artistId: newArtist.id,
     });
     global.io.emit(`${chat_id}`, { status: STEP_OF_ANALYSIS.SAVING_ANALYSIS });
-    await completeAnalysis(analysisId);
+    await updateAnalysisStatus(
+      chat_id,
+      analysisId,
+      STEP_OF_ANALYSIS.SAVING_ANALYSIS,
+    );
     await trackFunnelAnalysisChat(
       address,
       handle,
@@ -58,9 +66,7 @@ const getTikTokAnalysis = async (handle, chat_id, account_id, address) => {
       chat_id,
       "TikTok",
     );
-    global.io.emit(`${chat_id}`, {
-      status: STEP_OF_ANALYSIS.FINISHED,
-    });
+    await updateAnalysisStatus(chat_id, analysisId, STEP_OF_ANALYSIS.FINISHED);
     return;
   } catch (error) {
     console.log(error);
