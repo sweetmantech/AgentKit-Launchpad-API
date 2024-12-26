27e6a4a89c5371ca1256722b1401011a52103dd6
Va~~~dym <tech.eng.me@gmail.com>
2024-12-25 17:09:58 -0800

    fix: error

--- agents/getTwitterAnalysis.js
+++ agents/getTwitterAnalysis.js
@@ -12,6 +12,7 @@ import { Funnel_Type } from "../lib/funnels.js";
 import saveFunnelProfile from "../lib/supabase/saveFunnelProfile.js";
 import trackFunnelAnalysisChat from "../lib/stack/trackFunnelAnalysisChat.js";
 import saveFunnelArtist from "../lib/supabase/saveFunnelArtist.js";
+import getFormattedProfile from "../lib/twitter/getFormattedProfile.js";
 
 const scraper = new Scraper();
 
@@ -21,16 +22,7 @@ const getTwitterAnalysis = async (handle, chat_id, account_id, address) => {
     const analysisId = newAnalysis.id;
     await updateAnalysisStatus(chat_id, analysisId, STEP_OF_ANALYSIS.PROFILE);
     const scrappedProfile = await scraper.getProfile(handle);
-    console.log("ZIAD PROFILE", scrappedProfile);
-    const profile = {
-      avatar: scrappedProfile.avatar,
-      bio: scrappedProfile.biography,
-      followers: scrappedProfile.followersCount,
-      followings: scrappedProfile.followingCount,
-      name: scrappedProfile.username,
-      nickname: scrappedProfile.name,
-      region: scrappedProfile.location,
-    };
+    const profile = getFormattedProfile(scrappedProfile);
     await updateAnalysisStatus(
       chat_id,
       analysisId,
@@ -38,7 +30,6 @@ const getTwitterAnalysis = async (handle, chat_id, account_id, address) => {
     );
     const allTweets = await getAllTweets(scraper, handle);
     const comments = getTwitterComments(allTweets, analysisId);
-    console.log("ZIAD COMMENTS", comments);
     await saveFunnelComments(comments);
     await updateAnalysisStatus(chat_id, analysisId, STEP_OF_ANALYSIS.SEGMENTS);
     const segments = await getSegments(comments);
