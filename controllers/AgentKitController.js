9ddcd49886bd624822b2a30585c267fe9a14a674
Sweets Sweetman <sweetmantech@gmail.com>
2025-01-10 12:30:11 -0300

    /api/agentkit/run - run agentkit

--- controllers/AgentKitController.js
+++ controllers/AgentKitController.js
@@ -1,4 +1,18 @@
+import initializeAgent from "../lib/AgentKit/initializeAgent.js";
+import runAutonomousMode from "../lib/AgentKit/runAutonomousMode.js";
+
 export const run = async (req, res) => {
-  console.log("Hello World");
-  res.json({ status: "success" });
+  try {
+    console.log("Hello World");
+    const { agent, config } = await initializeAgent();
+    await runAutonomousMode(agent, config);
+    res.json({
+      status: "success",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+    }
+    process.exit(1);
+  }
 };
